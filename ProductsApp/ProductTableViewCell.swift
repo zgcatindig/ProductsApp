befacf993d3d336325391b328296e31861ded77d
Aira Nicole Natividad <aira.natividad@paymaya.com>
2026-02-12 11:33:59 +0800

    product table view cell

---
+++ ProductsApp/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  ProductsApp
+//
+//  Created by Aira Nicole Natividad on 2/12/26.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
