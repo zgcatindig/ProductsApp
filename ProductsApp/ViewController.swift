7ab8906ec369258c78cfa299edd550a07f4564ce
Zachary Catindig <zachary.catindig@maya.ph>
2026-02-12 11:00:31 +0800

    init

---
+++ ProductsApp/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ProductsApp
+//
+//  Created by Zachary Allen Catindig on 2/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        
+        
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductsTableViewCell")
+        return cell ?? UITableViewCell()
+    }
+    
+
+}
